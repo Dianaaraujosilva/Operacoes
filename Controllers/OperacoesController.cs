ceedd98bbc6bec0f896ce858263951f506749ebc
Dianaaraujosilva <102118579+Dianaaraujosilva@users.noreply.github.com>
2022-09-25 21:34:07 -0300

    Adicionando as outras operações( -,  /, *)

--- Controllers/OperacoesController.cs
+++ Controllers/OperacoesController.cs
@@ -15,8 +15,48 @@ public class OperacoesController : Controller
         ViewBag.N2 = n2;
         ViewBag.Resultado = n1 + n2;
 
+        return View();
+    }
+    
+    public IActionResult Subtrair() 
+    {
+        return View();
+    }
 
+    public IActionResult ResultadoSubtracao(double n1, double n2) 
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 - n2;
+        
+        return View();
+    }
 
+    public IActionResult Dividir() 
+    {
+        return View();
+    }
+
+    public IActionResult ResultadoDivisao(double n1, double n2) 
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 / n2;
+        
+        return View();
+    }
+
+    public IActionResult Multiplicar() 
+    {
+        return View();
+    }
+
+    public IActionResult ResultadoMultiplicacao(double n1, double n2) 
+    {
+        ViewBag.N1 = n1;
+        ViewBag.N2 = n2;
+        ViewBag.Resultado = n1 * n2;
+        
         return View();
     }
 }
